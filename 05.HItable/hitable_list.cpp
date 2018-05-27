68016e7c0e1fdc3c772a63ab1effcd56d6ba24f4
tusharsankhala <tusharsankhala@gmail.com>
2018-05-27 13:38:46 +0530

    Code updates

--- 05.HItable/hitable_list.cpp
+++ 05.HItable/hitable_list.cpp
@@ -18,8 +18,10 @@ bool hitable_list::hit(const Ray& r, float t_min, float t_max, hit_record& rec) 
 	hit_record temp_rec;
 	bool hit_anything = false;
 	double closest_so_far = t_max;
-	for (int i = 0; i < list_size; i++) {
-		if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) {
+	for (int i = 0; i < list_size; i++) 
+	{
+		if (list[i]->hit(r, t_min, closest_so_far, temp_rec)) 
+		{
 			hit_anything = true;
 			closest_so_far = temp_rec.t;
 			rec = temp_rec;
--- 05.HItable/vec3.h
+++ 05.HItable/vec3.h
@@ -1,5 +1,5 @@
-# ifndef VEC3H
-#define VEC3H
+#pragma once
+
 #include <math.h>
 #include <stdlib.h>
 #include <iostream>
@@ -17,19 +17,20 @@ public:
 	vec3 operator-(const vec3 &v2) const { return vec3(x - v2.x, y - v2.y, z - v2.z); }
 	vec3 operator*(const vec3 &v2) const { return vec3(x * v2.x, y * v2.y, z * v2.z); }
 	vec3 operator/(const vec3 &v2) const { return vec3(x / v2.x, y / v2.y, z / v2.z); }
+	vec3 operator/(const float f) const { return vec3(x / f, y / f, z / f); }
 
 	vec3& vec3::operator+=(const vec3 &v) {	x += v.x; y += v.y; z += v.z; return *this; }
 	vec3& vec3::operator*=(const vec3 &v) {	x *= v.x; y *= v.y; z *= v.z; return *this; }
 	vec3& vec3::operator/=(const vec3 &v) {	x /= v.x; y /= v.y;	z /= v.z; return *this; }
 	vec3& vec3::operator-=(const vec3& v) {	x -= v.x; y -= v.y; z -= v.z; return *this; }
 	vec3& vec3::operator*=(const float t) { x *= t;	y *= t;	z *= t;	return *this; }
-	vec3& vec3::operator/=(const float t) {	float k = 1.0 / t; x *= k; y *= k; z *= k; return *this; }
+	vec3& vec3::operator/=(const float t) {	float k = 1.0f / t; x *= k; y *= k; z *= k; return *this; }
 
 	inline float length() const { return sqrt(x * x + y * y + z * z); }
 	inline float squared_length() const { return x * x + y * y + z * z; }
 	inline void make_unit_vector()
 	{
-		float k = 1.0 / sqrt(x * x + y * y + z * z);
+		float k = 1.0f / sqrt(x * x + y * y + z * z);
 		x *= k; y *= k; z *= k;
 	}
 
@@ -41,16 +42,7 @@ public:
 	float x, y, z;
 };
 
-inline vec3 operator*(float t, const vec3 &v) {
-	return  vec3(t * v.x, t * v.y, t * v.z);
-}
-
-inline vec3 operator/(vec3 v, float t) 
-{
-	return  vec3(v.x / t, v.y / t, v.z / t);
-}
-
-inline vec3 operator*(const vec3 &v, float t) 
+inline vec3 operator*(float t, const vec3 &v)
 {
 	return  vec3(t * v.x, t * v.y, t * v.z);
 }
@@ -59,5 +51,3 @@ inline vec3 unit_vector(vec3 v)
 {
 	return v / v.length();
 }
-
-#endif
